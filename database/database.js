155caf59e12fe4fd192da6bb2cb61080b31ac267
CleranceDev <your-email@associated-with-clerancedev-account.com>
2025-06-18 01:48:41 +0200

    Connected to Railway MySQL and tested API endpoints

--- database/database.js
+++ database/database.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.HOST,
+  port: process.env.PORT_DB,      // <-- Add this line
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
